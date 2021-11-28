984022d68d5477f7da31f7f29d97ba5cc1ffa348
hakagashi1 <86728623+hakagashi1@users.noreply.github.com>
2021-11-28 17:17:26 +0700

    Database update

--- src/java/dao/ProjectDAO.java
+++ src/java/dao/ProjectDAO.java
@@ -5,10 +5,50 @@
  */
 package dao;
 
+import entity.Project;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ZenBook
  */
-public class ProjectDAO {
+public class ProjectDAO extends DBContext {
+    public List<Project> getAllProjects() {
+        List<Project> list = new ArrayList<>();
+        String sql = "SELECT * FROM Project";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Project p = new Project();
+                p.setId(rs.getInt("id"));
+                p.setTitle(rs.getString("title"));
+                p.setDescription(rs.getString("description"));
+                p.setTeam(new TeamDAO().getTeamById(rs.getInt("teamId")));
+                p.setCreatedBy(new UserDAO().getUserById(rs.getInt("createdBy")));
+                p.setUpdatedBy(new UserDAO().getUserById(rs.getInt("updatedBy")));
+                p.setStatus(rs.getInt("status"));
+                p.setHours(rs.getInt("hours"));
+                p.setCreateAt(rs.getDate("createdAt"));
+                p.setUpdatedAt(rs.getDate("updatedAt"));
+                p.setPlannedStartDate(rs.getDate("plannedStartDate"));
+                p.setPlannedEndDate(rs.getDate("plannedEndDate"));
+                p.setActualStartDate(rs.getDate("actualStartDate"));
+                p.setActualEndDate(rs.getDate("actualEndDate"));
+                p.setContent(rs.getString("content"));
+                list.add(p);
+            }
+        } catch (Exception e) {
+            System.err.print(e);
+        }
+        return list;
+    }
     
+    public static void main(String[] args) {
+        ProjectDAO p = new ProjectDAO();
+        System.out.println(p.getAllProjects());
+    }
 }
